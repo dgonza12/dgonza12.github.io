a09218ec410738a67de12b0e1af57664d098ebba
Danny Gonzalez <danny.gonzalez@regalix-inc.com>
2019-07-21 19:35:00 -0700

    update

--- js/script.js
+++ js/script.js
@@ -11,13 +11,16 @@ $(window).scroll(function (event) {
 
 $('.nav-bar li').on('click', function (event) {
     $(('.' + $(event.currentTarget).attr('data-section')))[0].scrollIntoView({behavior: 'smooth'});
+    $('body').removeClass('noScroll');
 });
 
 $('.timeline-item .expand, .timeline-item .close').on('click', function (event) {
     if($(this).parent().hasClass('active')){
         $('.timeline-item').removeClass('active');
+        $('body').removeClass('noScroll');
     } else {
         $('.timeline-item').removeClass('active');
         $(this).parent().addClass('active');
+        $('body').addClass('noScroll');
     }
 });
